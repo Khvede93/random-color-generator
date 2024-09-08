e6915ef821acb14b24af01a1cc1c2a6a3ad25964
Khvede93 <gio.khvede93@gmail.com>
2024-09-08 19:42:12 +0200

    prepare project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Add your React component files here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
